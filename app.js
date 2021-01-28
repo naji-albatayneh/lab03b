0f1a50ab87465692a45b0610946c4fffa5f759cc
Jane Smith <example@email.com>
2021-01-29 06:26:11 +0800

    bugs fixed

--- app.js
+++ app.js
@@ -5,27 +5,32 @@ var answer;
 var current = window.location.pathname;
 console.log(current);
 if(current == '/lab03b/index.html') {
-    for(var i=3;i>0;i--){
-        answer = prompt('You have ' + i + ' attempts to confirm that you are NOT A MACHINE. Please enter the result of (3 + 6) ', 'Type your name here..');
-        if(answer==9){
-            notMachine=true;
-            userName = prompt('Please enter your name ', 'Type your name here..');
-            age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + 'This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..');
-            
-            if(age<18){
-                document.getElementById("p1").innerHTML= '<h2 style="color: red;">' + 'Limited view for under 18 - You will not be able to view photos' + '</h2>';
-                alert('Hey ' + userName + '! We costomized our content to match your age. Have fun!');
-                document.getElementById("p2").innerHTML= '<h3 style="color: red;">' + 'Access to photos is denied for under 18' + '</h3>';
-                //document.getElementById("p2").textContent= "Access to photos is denied for under 18";
+    if(notMachine==false){
+        for(var i=3;i>0;i--){
+            answer = prompt('You have ' + i + ' attempts to confirm that you are NOT A MACHINE. Please enter the result of (3 + 6) ', 'Type your answer here..');
+            if(answer==9){
+                notMachine=true;
+                userName = prompt('Please enter your name ', 'Type your name here..');
+                age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + 'This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..');
+                
+                if(age<18){
+                    document.getElementById("p1").innerHTML= '<h2 style="color: red;">' + 'Limited view for under 18 - You will not be able to view photos' + '</h2>';
+                    alert('Hey ' + userName + '! We costomized our content to match your age. Have fun!');
+                    document.getElementById("p2").innerHTML= '<h3 style="color: red;">' + 'Access to photos is denied for under 18' + '</h3>';
+                    //document.getElementById("p2").textContent= "Access to photos is denied for under 18";
+                }else{
+                    document.getElementById("p1").innerHTML= '<h2 style="color: green;">' + 'Full access view for 18+' + '</h2>';
+                    alert('Hey ' + userName + '! You have full access to our content. Have fun!');
+                    document.getElementById("p2").innerHTML= '<img src="https://www.falken.com.au/wp-content/uploads/2019/10/James-Deane-2019-Formula-Drift-Champion-2-Copy.jpg" width="400px"; height="300px";/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://bloximages.chicago2.vip.townnews.com/gazettes.com/content/tncms/assets/v3/editorial/3/c4/3c4f3ff8-54d4-11e9-9fa0-c34c4ef96b86/58e2c1e32b8f7.image.jpg?resize=881%2C502" width="400px"; height="300px";/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://store-images.s-microsoft.com/image/apps.23298.14355368446062212.8bbd23ed-e9ab-4691-b02a-78fef05d4c17.a6d2007c-e955-4f7a-a2c8-b72fc76f1440?w=1399&h=874&q=90&format=jpg" width="400px"; height="300px";/>';
+                }
+            }else if(i==0){
+                alert('Your answer is not correct. You are considered a machine!');
             }else{
-                document.getElementById("p1").innerHTML= '<h2 style="color: green;">' + 'Full access view for 18+' + '</h2>';
-                alert('Hey ' + userName + '! You have full access to our content. Have fun!');
-                document.getElementById("p2").innerHTML= '<img src="https://www.falken.com.au/wp-content/uploads/2019/10/James-Deane-2019-Formula-Drift-Champion-2-Copy.jpg" width="400px"; height="300px";/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://bloximages.chicago2.vip.townnews.com/gazettes.com/content/tncms/assets/v3/editorial/3/c4/3c4f3ff8-54d4-11e9-9fa0-c34c4ef96b86/58e2c1e32b8f7.image.jpg?resize=881%2C502" width="400px"; height="300px";/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://store-images.s-microsoft.com/image/apps.23298.14355368446062212.8bbd23ed-e9ab-4691-b02a-78fef05d4c17.a6d2007c-e955-4f7a-a2c8-b72fc76f1440?w=1399&h=874&q=90&format=jpg" width="400px"; height="300px";/>';
+                alert('Your answer is not correct. Please try again');
             }
-        }else{
-            alert('Your answer is not correct. Please try again');
         }
     }
+    
     if(notMachine=false){
         document.getElementById("m1").innerHTML= '<p style="color: red;">' + 'Access denied. You are cosidered a machine!' + '</p>';
         document.getElementById("m2").innerHTML= '<h2 style="color: red;">' + 'Access denied. You are cosidered a machine!' + '</h2>';
