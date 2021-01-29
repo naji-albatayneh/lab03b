c2111cc9016dbb7adeb9a5d6e142e802183e034d
Jane Smith <example@email.com>
2021-01-29 19:00:25 +0800

    adding function

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var notMachine=false;
 var answer;
 var current = window.location.pathname;
 console.log(current);
+
+function userInput(){
+    userName = prompt('Please enter your name ', 'Type your name here..');
+    age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + 'This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..');
+}
+
 if(current == '/lab03b/index.html') {
     console.log('at homepage');
     var i=3;
@@ -12,8 +18,9 @@ if(current == '/lab03b/index.html') {
             answer = prompt('You have ' + i + ' attempts to confirm that you are NOT A MACHINE. Please enter the result of (3 + 6) ', 'Type your answer here..');
             if(answer==9){
                 notMachine=true;
-                userName = prompt('Please enter your name ', 'Type your name here..');
-                age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + 'This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..');
+                userInput();
+                //userName = prompt('Please enter your name ', 'Type your name here..');
+                //age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + 'This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..');
                 document.getElementById("m1").innerHTML ='<ul><li style="display: inline;"><a href="index.html">Home</a></li><li style="display: inline;">|</li><li style="display: inline;"><a href="gallary.html">Photo Gallary</a></li><li style="display: inline;">|</li><li style="display: inline;"><a href="contactus.html">Contact Us</a></li><li style="display: inline;">|</li><li style="display: inline;"><a href="labmates.html">View Other Sports by Lab03b Mates</a></li></ul>';
                 document.getElementById("m2").innerHTML ='<p>Formula D or Formula DRIFT Black Magic Pro Championship Presented by BlackVue Dash Cameras is the premier United States-based drifting series. Formula Drift, Inc. was co-founded by Jim Liaw and Ryan Sage in 2003 as a sister company to the now defunct Slipstream Global Marketing, the same partnership that introduced D1 Grand Prix to the United States. The new entity would solely own, operate and launch the first official drifting series in North America. 2018 was Formula Drifts fifteenth year of competition. With 60 currently licensed drivers competing in Pro and Pro 2, Formula Drift is recognized as the premier North American professional drifting championship series. The series consists of an eight-round championship played out at race tracks across North America. Judged on line, angle, and style, rather than who finishes the course in the fastest time, Formula Drift brings together traditional racing and extreme sports. Formula Drift works closely with a huge variety of sponsors such as: Black Magic Car Care, BlackVue Dash Cameras, NOS Energy Drink, and the United States Air Force. Most notably Formula Drift works with the largest amount of tire suppliers to a single series. Achilles Radial, Falken Tire, Hankook Tire, Nexen Tire, and Nitto Tire all work together to supply teams and develop test their products.</p>';
                 document.getElementById("p2").innerHTML= '<img src="https://www.falken.com.au/wp-content/uploads/2019/10/James-Deane-2019-Formula-Drift-Champion-2-Copy.jpg" width="400px"; height="300px";/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://bloximages.chicago2.vip.townnews.com/gazettes.com/content/tncms/assets/v3/editorial/3/c4/3c4f3ff8-54d4-11e9-9fa0-c34c4ef96b86/58e2c1e32b8f7.image.jpg?resize=881%2C502" width="400px"; height="300px";/>&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://store-images.s-microsoft.com/image/apps.23298.14355368446062212.8bbd23ed-e9ab-4691-b02a-78fef05d4c17.a6d2007c-e955-4f7a-a2c8-b72fc76f1440?w=1399&h=874&q=90&format=jpg" width="400px"; height="300px";/>';
@@ -41,8 +48,9 @@ if(current == '/lab03b/index.html') {
     //document.getElementById("m2").innerHTML= '<main><h2 style="color: red;">' + 'Access denied. You are cosidered a machine!' + '</h2></main>';
 
 } else if(current == '/lab03b/gallary.html') {
-    userName = prompt('Please enter your name ', 'Type your name here..');
-    age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + ' This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..')
+    userInput();
+    //userName = prompt('Please enter your name ', 'Type your name here..');
+    //age = prompt('Welcome to Formula Drift Website, ' + userName + '!' + ' This site contains media showing Profissional Drifers drifting their cars. Please do not try doing what you see here. Please enter your age to continue.' , 'Your age..')
     if(age<18){
         alert('Hey ' + userName + '! We costomized our content to match your age. Have fun!');
         document.getElementById("p4").innerHTML= '<h2 style="color: red;">' + 'Limited view for under 18 - You will not be able to view photos' + '</h2>';
